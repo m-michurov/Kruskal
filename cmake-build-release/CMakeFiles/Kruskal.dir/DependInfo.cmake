51128cbcc5b317d701ba5e766e2752c98029684b
m-michurov <>
2019-03-18 17:34:29 +0700

    release

---
+++ cmake-build-release/CMakeFiles/Kruskal.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/CLionProjects/Kruskal/disjointsets.c" "E:/CLionProjects/Kruskal/cmake-build-release/CMakeFiles/Kruskal.dir/disjointsets.c.obj"
+  "E:/CLionProjects/Kruskal/kruskal.c" "E:/CLionProjects/Kruskal/cmake-build-release/CMakeFiles/Kruskal.dir/kruskal.c.obj"
+  "E:/CLionProjects/Kruskal/main.c" "E:/CLionProjects/Kruskal/cmake-build-release/CMakeFiles/Kruskal.dir/main.c.obj"
+  "E:/CLionProjects/Kruskal/qsort.c" "E:/CLionProjects/Kruskal/cmake-build-release/CMakeFiles/Kruskal.dir/qsort.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release/CMakeFiles/Kruskal.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kruskal.dir/main.c.obj"
+  "CMakeFiles/Kruskal.dir/disjointsets.c.obj"
+  "CMakeFiles/Kruskal.dir/qsort.c.obj"
+  "CMakeFiles/Kruskal.dir/kruskal.c.obj"
+  "Kruskal.pdb"
+  "Kruskal.exe"
+  "Kruskal.exe.manifest"
+  "libKruskal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Kruskal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
